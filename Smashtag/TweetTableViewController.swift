bc7347107e156ed2e57f4373221b08836b99d52b
KimYusung <yusungkim@me.com>
2017-09-10 21:59:37 +0900

    Twitter frame added.

--- Smashtag/TweetTableViewController.swift
+++ Smashtag/TweetTableViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import Twitter
 
 class TweetTableViewController: UITableViewController {
 
     // MARK: Model
-    private var tweets = [Array<Tweet>]() // array of array of Tweet
+    private var tweets = [Array<Twitter.Tweet>]() // array of array of Tweet
     
     var searchText: String? {
         didSet {
@@ -19,10 +20,36 @@ class TweetTableViewController: UITableViewController {
             tableView.reloadData()
             searchForTweets()
             title = searchText
+            print(tweets) // for testing
         }
     }
     
+    private func twitterRequest() -> Twitter.Request? {
+        if let query = searchText, !query.isEmpty { // searchText is neither nil nor empty
+            return Twitter.Request(search: query, count: 100)
+        }
+        return nil
+    }
+    
+    private var currentRequest: Twitter.Request?
     private func searchForTweets() {
+        if let request = twitterRequest() {
+            currentRequest = request
+            
+            // request.fetchTweets( ([Tweet]) -> void )
+            // takes closure, a function, which is excuted when the fetch job is done.
+            // because, the fetch is worked on a different queue.
+            request.fetchTweets { [weak self] newTweets in
+                
+                if request == self?.currentRequest { // check the request is still the same, even after the fetching.
+                    self?.tweets.insert(newTweets, at: 0)
+                } else {
+                    // ignore, because the request has been changed.
+                    // currentRequest is changed to more recent request on the main queue.
+                    // request in this queue is no more relevent.
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
